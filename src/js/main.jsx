7ab67c4c094fb213fa3e2e87e2d76656f07bcd0a
Mchaiban <marianafeels@gmail.com>
2025-04-28 19:10:58 +0000

    First commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon }              from '@fortawesome/react-fontawesome';
+import './icons.js';
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
-// index.css'
-import '../styles/index.css'
+// Your global styles
+import '../styles/index.css';
 
-// components
-import Home from './components/Home';
+function SimpleCounter({ digitFour, digitThree, digitTwo, digitOne }) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+      <FontAwesomeIcon icon="fa-solid fa-clock" />
+      </div>
+      <div className="four">{digitFour}</div>
+      <div className="three">{digitThree}</div>
+      <div className="two">{digitTwo}</div>
+      <div className="one">{digitOne}</div>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+SimpleCounter.propTypes = {
+  digitFour:  PropTypes.number.isRequired,
+  digitThree: PropTypes.number.isRequired,
+  digitTwo:   PropTypes.number.isRequired,
+  digitOne:   PropTypes.number.isRequired,
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let counter = 0;
+function tick() {
+  counter++;
+  const four  = Math.floor(counter / 1000) % 10;
+  const three = Math.floor(counter /  100) % 10;
+  const two   = Math.floor(counter /   10) % 10;
+  const one   = Math.floor(counter /    1) % 10;
+
+  root.render(
+    <React.StrictMode>
+      <SimpleCounter
+        digitFour  ={four}
+        digitThree ={three}
+        digitTwo   ={two}
+        digitOne   ={one}
+      />
+    </React.StrictMode>
+  );
+}
+
+tick();
+setInterval(tick, 1000);
